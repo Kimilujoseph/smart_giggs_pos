03a4426449e02692e4c8bcdc7a947a2bb3a04498
timothy joseph <timothyjoseph8580@gmail.com>
2026-05-26 13:16:59 +0300

    created a new config file for the production environment

---
+++ production_ecosystem.config.cjs
@@ -0,0 +1,40 @@
+require("dotenv").config({ path: "./.env" });
+
+module.exports = {
+  apps: [
+    {
+      // General
+      name: "T10stores_production",
+      script: "src/index.js",
+
+      // Production Environment
+      env_production: {
+        NODE_ENV: "production",
+        ...process.env,
+      },
+
+      // Clustering
+      exec_mode: "cluster",
+      instances: 3,
+
+      autorestart: true,
+      watch: false,
+      max_memory_restart: "1G",
+
+      // Log Management
+      log_date_format: "YYYY-MM-DD HH:mm:ss Z",
+      output: "./logs/out-production.log",
+      error: "./logs/error-production.log",
+      merge_logs: true,
+    },
+    {
+      name: "cron_jobs_app_production",
+      script: "./src/cron_jobs_app.js",
+      instances: 1,
+      exec_mode: "fork",
+      watch: false,
+      output: "./logs/cron-out-production.log",
+      error: "./logs/cron-error-production.log",
+    },
+  ],
+};
