a659fa43aa70019d01dbe237e1541e91bc9c5f09
kimilujoseph <timothyjoseph8580@gmail.com>
2026-07-21 20:55:34 +0300

    added an executable file for the browser

--- src/Utils/BrowserPool.js
+++ src/Utils/BrowserPool.js
@@ -30,6 +30,7 @@ class BrowserPool {
         try {
             const browser = await puppeteer.launch({
                 headless: true,
+                executablePath: "/usr/bin/chromium",
                 args: [
                     "--no-sandbox",
                     "--disable-setuid-sandbox",
